7ce6a69573eb725fe58fffdb1b344eb97b7f7b79
Emma Macey <macey26@hotmail.com>
2018-01-30 13:03:17 +1100

    update repeat string and helloWorld.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-var repeatString = function() {
-
+var repeatString = function(str) {
+ var str = "hey";
+for (var i = 0; i < 3; i++) {
+		
+  str.repeat();
+        return str;
 }
+	}	
+	
+    
+
+repeatString();
 
 module.exports = repeatString
