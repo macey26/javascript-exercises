4bf41e5f43cb875d254adf8635cf77d7f2a73536
Emma Macey <macey26@hotmail.com>
2018-02-05 23:17:47 +1100

    working on arguemts

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -9,7 +9,7 @@ var removeFromArray = function(...args) {
   var arr = args[0];
 
 arr = arr.filter(function(value, index){
-	return args.indexOf(index) == -1;
+	return args.indexOf(index) == args;
 });
 	
 };
