dea732ceec80a7c84c2cd6783aa8c7e0ea8717e8
Emma Macey <macey26@hotmail.com>
2018-02-08 12:31:02 +1100

    created for loop

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,14 @@
-var sumAll = function() {
+var sumAll = function(arr) {
 
+	finalSum = 0;
+
+  for (i =0; i < arr.length; i++){
+
+  	finalSum += i;
 }
+return finalSum;
+}
+
+  sumAll([1, 4]);
 
 module.exports = sumAll
