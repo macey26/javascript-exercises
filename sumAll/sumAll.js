e6ac3dfd43621a24661f397450a5ea82b4ad8e7b
Emma Macey <macey26@hotmail.com>
2018-02-17 00:01:52 +1100

    passed negative number test

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,10 +1,23 @@
 function sumAll (first , final){
 	finalSum = 0;
 
+	if(first < 0 || final < 0){
+		return "ERROR";
+	}
+
+	if(final > first){
   for (i = first; i <= final; i++){
 
   	finalSum += i;
-}
+      }
+    } else if (first > final ){
+  for (i = final; i <= first; i++){
+
+  	finalSum += i;
+}   	
+
+
+    }
 return finalSum;
 }
 
