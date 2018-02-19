22257e3e2dbca9f669733fcf1890ba40cbb02835
Emma Macey <macey26@hotmail.com>
2018-02-19 14:59:52 +1100

    passed all tests for leap years

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,4 +1,19 @@
-var leapYears = function() {
+var leapYears = function(year) {
+
+	if((year %4 === 0) && year %100 !== 0){
+		return true;
+         
+        } else if(year %400 == 0){
+
+            return true;
+        } else {
+
+         	return false;
+         }
+	  
+
+   
+
 
 }
 
