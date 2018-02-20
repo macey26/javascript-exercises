eb55887bd3ea5e0eefc31426841765a805be17a4
Emma Macey <macey26@hotmail.com>
2018-02-20 13:06:24 +1100

    temp conversion solve

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,18 +1,20 @@
 var ftoc = function(fahrenheit) {
 
-	var celsius;
+	
 
-	celsius = fahrenheit - 32 * 5 / 9;
+	 var celsius = Math.round((fahrenheit - 32) * 5 / 9 * 10) / 10;
 
     return celsius;
   
 }
 
+ftoc();
+
 
 var ctof = function(celsius) {
-    var fahrenheit;
+   
 
-  fahrenheit = celsius * 9/5 + 32;
+var fahrenheit = Math.round(((celsius * 9/5 ) + 32) * 10) / 10;
   return fahrenheit;
 }
 
--- tempConversion/tempConversion.spec.js
+++ tempConversion/tempConversion.spec.js
@@ -4,10 +4,10 @@ describe('ftoc', function() {
   it('works', function() {
     expect(ftoc(32)).toEqual(0);
   });
-  xit('rounds to 1 decimal', function() {
+  it('rounds to 1 decimal', function() {
     expect(ftoc(100)).toEqual(37.8);
   });
-  xit('works with negatives', function() {
+  it('works with negatives', function() {
     expect(ftoc(-100)).toEqual(-73.3);
   });
 });
@@ -19,7 +19,7 @@ describe('ctof', function() {
   it('rounds to 1 decimal', function() {
     expect(ctof(73.2)).toEqual(163.8);
   });
-  xit('works with negatives', function() {
+  it('works with negatives', function() {
     expect(ctof(-10)).toEqual(14);
   });
 });
