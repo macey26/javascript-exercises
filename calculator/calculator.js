244b165747ee86e0868e5f32187b05270ae85bc2
Emma Macey <macey26@hotmail.com>
2018-04-28 12:23:31 +1000

    completed factorilise number tests

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -25,8 +25,14 @@ function power(a,  b) {
    return Math.pow(a, b);
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if (num === 0 || num ===1) {
+		return 1;
+       }
+	for (var i = num - 1; i >= 1; i--) {
+		num *= i;	
+		}	
+	return num
 }
 
 module.exports = {
