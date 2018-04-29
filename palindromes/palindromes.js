7486dfc243f0fbd034f181cfe7e6c1001c74e683
Emma Macey <macey26@hotmail.com>
2018-04-29 14:56:21 +1000

    completed code

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,19 @@
-var palindromes = function() {
+var palindromes = function(str) {
 
+ var replacer = /[^A-Za-z0–9]/g;
+
+  var lowerCaseReplace = str.toLowerCase().replace(replacer, '');
+
+ var splitReverse = lowerCaseReplace.split("").reverse().join("");
+
+if (lowerCaseReplace === splitReverse) {
+
+	return true;
+} else {
+
+	return false;
 }
+
+ }
 
 module.exports = palindromes
