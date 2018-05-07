3512d19ad99400edb985fb82254f23e043f5267b
Emma Macey <macey26@hotmail.com>
2018-05-07 20:29:26 +1000

    competed snakeCase

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,4 +1,12 @@
-var snakeCase = function() {
+var snakeCase = function(str) {
+	var answer =  str;
+
+		if(str.match(/^[a-z]/)){
+			answer = str.replace(/([a-z]+)([A-Z])|[_|\s]+/g, '$1_$2');
+		}
+
+	return answer.replace(/[.?!,-]/g, ' ').replace(/\s+/g, ' ').trim().replace(/ /g, '_').toLowerCase(); 
+
 
 }
 
