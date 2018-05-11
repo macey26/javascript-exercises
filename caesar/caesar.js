d9169f86c20ecd5febdf22c9304aac508e21f670
Emma Macey <macey26@hotmail.com>
2018-05-11 12:41:57 +1000

    completed Caesar exercises.

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,4 +1,34 @@
-var caesar = function() {
+var caesar = function(str, places) {
+
+	if (places < 0){
+		return caesar(str, places + 26);
+	}
+
+	var answer = '';
+  
+  for(var i = 0; i < str.length; i++){
+
+  	var character = str[i];
+
+  	if (character.match(/[a-z]/i)) {
+
+
+
+  	if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90){
+  		character = String.fromCharCode(((str.charCodeAt(i) - 65 + places) % 26) + 65);
+
+    }else if(str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122){
+      character = String.fromCharCode(((str.charCodeAt(i) - 97 + places) % 26) + 97 );
+    }
+
+  	}
+
+  	answer += character;
+   
+  }
+  
+  return answer;
+
 
 }
 
