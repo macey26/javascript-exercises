6ef5e6a4898507ccd03474498d34f532263f2cab
Emma Macey <macey26@hotmail.com>
2018-05-17 21:15:40 +1000

    Completed fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-var fibonacci = function() {
+var fibonacci = function(num) {
+	  var first = 0, second = 1, next = 1;
 
+	  if (num < 0){
+	  	return "OOPS"
+	  }
+  
+  for(var i = 2; i <= num; i++){
+    next = first + second;
+    first = second;
+    second = next; 
+  }
+  return next;
 }
+
 
 module.exports = fibonacci
