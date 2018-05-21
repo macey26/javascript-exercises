b0d8d59de5aee4be74b7aea53116ecb3b2cac572
Emma Macey <macey26@hotmail.com>
2018-05-21 20:40:22 +1000

    passed first 2 tests on exercise

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,6 +1,24 @@
-function translate() {
-	// body...
+function translate(str) {
+	var pigLatin = [];
+	var regex = /[aeiou]/gi
+	var tempArr = "";
+
+
+if (str[0].match(regex)){
+	 return str + "ay";
+  } else {
+  	  pigLatin = str.split("")
+
+  	 tempArr = pigLatin.shift()
+  	  pigLatin.push(tempArr);
+  	   
+   }
+
+   return pigLatin.join("")+"ay";
+   console.log(pigLatin) ;
 }
+
+translate("banana");
 
 
 module.exports = {
